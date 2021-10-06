f64637a938e9376487bab95a66eb4a5e2633b217
Amit Kumar Umarvaishya <amitkumar.u@optum.com>
2021-10-06 13:35:29 +0530

    Bowling_Game_assignment

---
+++ Bowling-ball-Amit/BowlingGame.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bowling_ball_Amit
+{
+    class BowlingGame
+    {
+        private readonly Frame[] frames = new Frame[10];
+        private int totalScore;
+        private int frameCounter = 0;
+
+        public BowlingGame()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Frame frame = new Frame();
+                frames[i] = frame;
+            }
+        }
+        public int GetTotalScore()
+        {
+            for (int frameIndex = 0; frameIndex < 10; frameIndex++)
+            {
+                Frame frame = frames[frameIndex];
+                int currentFrameScore = frame.GetFrameScore();
+                if (frame.IsStrike() || frame.IsSpare())
+                {
+                    if (frameIndex == 9)
+                    {
+                        currentFrameScore += frames[frameIndex].LastRollScore;
+                    }
+                    else
+                    {
+                        currentFrameScore += GetBonusScore(frameIndex);
+                    }
+                }
+                totalScore += currentFrameScore;
+            }
+            return totalScore;
+        }
+
+        private int GetBonusScore(int frameIndex)
+        {
+            int bonus = 0;
+            if (frames[frameIndex].IsStrike())
+            {
+                bonus = frames[frameIndex + 1].GetFrameScore();
+            }
+            else if (frames[frameIndex].IsSpare())
+            {
+                bonus = frames[frameIndex + 1].GetFirstRollScore();
+            }
+            return bonus;
+        }
+
+        public void Roll(int pins)
+        {
+            Frame frame = frames[frameCounter];
+            frame.SetFrameScores(pins);
+            frames[frameCounter] = frame;
+            if (frameCounter < 9 && (frame.IsStrike() || frame.FrameRollCount > 2))
+                frameCounter++;
+        }
+
+        class Frame
+        {
+            public int LastRollScore { get; set; }
+            public int FrameRollCount = 1;
+            private int[] scores = new int[2];
+
+            public void SetFrameScores(int pins)
+            {
+                if (FrameRollCount == 3)
+                {
+                    LastRollScore = pins;
+                }
+                else
+                {
+                    scores[FrameRollCount - 1] = pins;
+                }
+                //FrameScore += pins;
+                FrameRollCount++;
+            }
+
+            internal int GetFrameScore()
+            {
+                return scores[0] + scores[1];
+            }
+
+            internal int GetFirstRollScore()
+            {
+                return scores[0];
+            }
+
+            internal bool IsStrike()
+            {
+                if (scores[0] == 10)
+                    return true;
+                else
+                    return false;
+
+            }
+
+            internal bool IsSpare()
+            {
+                if (scores[0] + scores[1] == 10)
+                    return true;
+                else
+                    return false;
+
+            }
+        }
+    }
+}
---
+++ Bowling-ball-Amit/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Bowling_ball_Amit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome to Bowling game:");
+                Console.WriteLine("Enter Y for Random Roll entry else press any key and hit enter");
+                string selection = Console.ReadLine();
+                BowlingGame game = new BowlingGame();
+                if (selection=="Y" || selection == "y")
+                {
+                    RandomRoll(game);
+                }
+                else
+                {
+                    ////SET 1
+                    game.Roll(6);
+                    game.Roll(4);
+
+                    game.Roll(5);
+                    game.Roll(5);
+
+                    game.Roll(1);
+                    game.Roll(4);
+
+                    game.Roll(4);
+                    game.Roll(5);
+
+                    game.Roll(10);
+
+                    game.Roll(0);
+                    game.Roll(1);
+
+                    game.Roll(7);
+                    game.Roll(3);
+
+                    game.Roll(6);
+                    game.Roll(4);
+
+                    game.Roll(9);
+                    game.Roll(1);
+
+                    game.Roll(4);
+                    game.Roll(0);
+                }
+                Console.WriteLine("Score: " + game.GetTotalScore());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+
+        private static void RandomRoll(BowlingGame game)
+        {
+            try
+            {
+                Random random = new Random();
+                int pins;
+                int totalpins;
+                for (int i = 1; i <= 10; i++)
+                {
+                    pins = 0;
+                    totalpins = 0;
+                    pins = random.Next(0, 10);
+                    game.Roll(pins);
+                    totalpins += pins;
+                    if (i < 10)
+                    {
+                        if (pins < 10)
+                        {
+                            pins = random.Next(0, 10 - pins);
+                            game.Roll(pins);
+                        }
+                    }
+                    else
+                    {
+
+                        if (pins < 10)
+                        {
+                            pins = random.Next(0, 10 - pins);
+                            game.Roll(pins);
+                            if (totalpins == 10)
+                            {
+                                pins = random.Next(0, 10);
+                                game.Roll(pins);
+                            }
+                        }
+                        else
+                        {
+                            pins = random.Next(0, 10);
+                            game.Roll(pins);
+                            pins = random.Next(0, 10);
+                            game.Roll(pins);
+                        }
+                    }
+
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
